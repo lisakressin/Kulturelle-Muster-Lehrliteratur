5e411c313798e45591ec16ae6c435271c22857b3
Lisa Kressin <lisa.kressin@protonmail.com>
2020-08-28 15:31:25 +0200

    Update 20192801_04_SyllCom_Litnets.py

--- 20192801_04_SyllCom_Litnets.py
+++ 20192801_04_SyllCom_Litnets.py
@@ -4,11 +4,9 @@ Created on Thu Jan 17 07:48:34 2019
 
 @author: KressinL
 
-In diesem Skript werden basierend auf dem bipartiten Netzwerk B Syllabi- und Literatur
-netzwerke erstellt. Erstere werden auf ihre enthaltenen Communities untersucht, die
+In diesem Skript werden basierend auf dem bipartiten Netzwerk Syllabi- und Literaturnetzwerke erstellt. 
+Erstere werden auf ihre enthaltenen Communities untersucht, die
 anschliessend den Rahmen für die Auswertung ihrer Literatur(netzwerke) bilden.
-Dies ist im Kern die Auswertung des Artikels "Aus 4 mach 3", welcher für den Projektretreat
-im Februar 2019 erstell worden ist.
 """
 import os
 import pandas as pd
@@ -18,8 +16,6 @@ from networkx.algorithms import community
 import community
 from operator import itemgetter
 import pickle
-
-os.chdir("P:\SWITCHdrive\Datenauswertung\Python_Zeug\Zitationsnetzwerke\Outputs\Objects")
 
 # Laden des bipartiten Graphen
 with open('B', 'rb') as B_file:
@@ -38,7 +34,6 @@ plaene = [x for x,y in B.nodes(data=True) if y['bipartite']==0]
 Gplaene = bi.weighted_projected_graph(B, plaene,ratio=True)
 
 # Hinzufügen von Attributen, die die Identifikation der Pläne ermöglichen.
-os.chdir("P:\SWITCHdrive\Datenauswertung\Python_Zeug\Zitationsnetzwerke\Outputs\Tables")
 attr_df = pd.read_csv("20192801_Ref_pro_Lehrplan.csv", index_col = False)
 
 ######################## Netzwerk- und Knotenmasse ###########################
@@ -49,15 +44,6 @@ print("Network density:", round(density, 3))
 
 # Anzahl an verbundenen Komponenten = 3, entsprechend liegt ein 'disconnected graph' vor
 nx.algorithms.components.number_connected_components(Gplaene)
-
-# Subgraphen erstellen
-#comp = list(nx.connected_component_subgraphs(Gplaene))[0]
-#len(list(comp.nodes)) # 168 - entsprechend habe ich eine grosse Component und 
-# zwei einzelne unverbundene Knoten
-# Dichte
-#density = nx.density(comp)
-#print("Network density:", round(density, 3))
-# 33 %
 
 # Zentralitätsmasse
 # degree
@@ -97,22 +83,14 @@ set(communities.values()) # 5 Communities
 nx.set_node_attributes(Gplaene, communities, 'modularity')
 
 # Graph für die Visualisierung speichern
-os.chdir("P:\SWITCHdrive\Datenauswertung\Python_Zeug\Zitationsnetzwerke\Outputs\Objects")
 with open('Plaenegraph', 'wb') as Gplaene_file:
     pickle.dump(Gplaene, Gplaene_file)
-
-# Für schnelle Gephiinspektion
-#os.chdir("P:\SWITCHdrive\Datenauswertung\Python_Zeug\Zitationsnetzwerke\Outputs\Gephi")
-#nx.write_graphml(Gplaene, "Syllabigraph.graphml")
 
 # Umwandlung der Nodesattribute in ein Dataframe, auch für Inspektion
 tab = pd.DataFrame.from_dict(dict(Gplaene.nodes(data=True)), orient='index')
 # Löschen der "bipartite"-Spalte, Rücksetzen des Index, Umbenennung von Spalten Sortierung der Zeilen nach Communityzugehörigkeit.
 tab = tab.drop(['bipartite'], axis =1).reset_index().rename(columns = 
               {'index': 'Syllabus ID'}).sort_values(by=['modularity'], ascending=True).rename(columns={'modularity':'Community'})
-
-# os.chdir("P:\SWITCHdrive\Datenauswertung\Python_Zeug\Zitationsnetzwerke\Outputs\Tables")
-# tab.to_csv("Syllabicommunities_Attr.csv", header=True, encoding='utf-8-sig', index=False)
 
 # Statistiken pro Communities
 tab.groupby('Community')['Uni'].value_counts()
@@ -130,11 +108,9 @@ kat = round(tab_noCS.groupby('Community')['Kategorie'].value_counts(normalize=Tr
 kat.name = 'prop. Anteil'
 kat = (kat*100).astype(int)
 # Speichern der Tabelle
-os.chdir("P:\SWITCHdrive\Datenauswertung\Python_Zeug\Zitationsnetzwerke\Outputs\Tables")
 kat.to_csv('20192801_Kategorie_pro_Com.csv', header= True)
 
 # Für die Visualisierung als Objekt speichern
-os.chdir("P:\SWITCHdrive\Datenauswertung\Python_Zeug\Zitationsnetzwerke\Outputs\Objects")
 with open('Kategorie_pro_Com', 'wb') as kat_file:
     pickle.dump(kat, kat_file)
 
@@ -174,7 +150,6 @@ for i in bottom_nodes0:
         
 nx.set_node_attributes(subG0, bi_dict0, 'bipartite')
 
-os.chdir("P:\SWITCHdrive\Datenauswertung\Python_Zeug\Zitationsnetzwerke\Outputs\Objects")
 with open('Sub_Com1', 'wb') as Sub_Com1_file:
     pickle.dump(subG0, Sub_Com1_file)
 
@@ -194,7 +169,6 @@ for i in bottom_nodes1:
         
 nx.set_node_attributes(subG1, bi_dict1, 'bipartite')
 
-os.chdir("P:\SWITCHdrive\Datenauswertung\Python_Zeug\Zitationsnetzwerke\Outputs\Objects")
 with open('Sub_Com2', 'wb') as Sub_Com2_file:
     pickle.dump(subG1, Sub_Com2_file)
 ######################## 3. Subgraph #####################################
@@ -213,7 +187,6 @@ for i in bottom_nodes2:
         
 nx.set_node_attributes(subG2, bi_dict2, 'bipartite')
 
-os.chdir("P:\SWITCHdrive\Datenauswertung\Python_Zeug\Zitationsnetzwerke\Outputs\Objects")
 with open('Sub_Com3', 'wb') as Sub_Com3_file:
     pickle.dump(subG2, Sub_Com3_file)
 
@@ -222,7 +195,6 @@ with open('Sub_Com3', 'wb') as Sub_Com3_file:
 # =============================================================================
 
 ############ Literaturnetzwerk des 1 Subgraphen (Community) ###################
-os.chdir("P:\SWITCHdrive\Datenauswertung\Python_Zeug\Zitationsnetzwerke\Outputs\Objects")
 with open('Sub_Com1', 'rb') as Sub_Com1_file:
     subG0 = pickle.load(Sub_Com1_file)
 
@@ -241,7 +213,6 @@ degree_dict0 = dict(Glit0.degree(Glit0.nodes))
 nx.set_node_attributes(Glit0, degree_dict0, 'degree')
 
 # Speichern des Literaturnetzwerkes des 1. Subgraphen
-os.chdir("P:\SWITCHdrive\Datenauswertung\Python_Zeug\Zitationsnetzwerke\Outputs\Objects") 
 with open('LitGraph_com0', 'wb') as Glit0_file:
     pickle.dump(Glit0, Glit0_file)
 
@@ -265,7 +236,6 @@ top5_0 = pd.DataFrame(
      'Gradzahl': grad0,
     })
 
-os.chdir("P:\SWITCHdrive\Datenauswertung\Python_Zeug\Zitationsnetzwerke\Outputs\Tables") 
 top5_0.to_csv("20192801_Top5_Com0.csv", header=True, encoding='utf-8-sig', index=False)
 
 # closeness
@@ -277,22 +247,16 @@ print("Top 5 nodes by Closeness Centrality:")
 for d in sorted_degree0[:5]:
     print(d)
 
-# Graph für Gephiinspektion speichern
-#os.chdir("P:\SWITCHdrive\Datenauswertung\Python_Zeug\Zitationsnetzwerke\Outputs\Gephi")
-#nx.write_graphml(Glit0, "20192801_Lit_SubG0.graphml")
-
 # Tabelle der Referenzen der ersten Community (Subgraphen) mit Zentralitätsmassen, sortiert nach Grad.
 lit0_counts = pd.DataFrame.from_dict(dict(Glit0.nodes(data=True)), orient='index')
 lit0_counts = lit0_counts.drop(['bipartite'], axis =1).reset_index().sort_values(by=['degree'], ascending=False).rename(columns={'degree':'Grad', 'index': 'Quelle'})
 lit0_counts['Closeness'] = round(lit0_counts['Closeness'], 2)
 
 # Speichern
-os.chdir("P:\SWITCHdrive\Datenauswertung\Python_Zeug\Zitationsnetzwerke\Outputs\Tables")
 lit0_counts.to_csv("20192801_Lit0_measures.csv", header=True, encoding='utf-8-sig', index=False)
 
 ###### Statistiken zur Literatur der ersten Community
 # Hinzufügen von Literatur-Attributen
-os.chdir("P:\SWITCHdrive\Datenauswertung\Python_Zeug\Zitationsnetzwerke\Outputs\Tables")
 attr_df = pd.read_csv("20192801_Ref_pro_Lehrplan.csv", index_col = False)
 # Entfernen aller Variablen, die die Lehrpläne statt die Referenzen charakterisieren.
 attr_df = attr_df.drop(['recnr', 'uni', 'modultitel', 'kurstitel', 'software', 'kategorie'], axis=1)
@@ -304,7 +268,6 @@ ref_attr0 = pd.DataFrame.from_dict(dict(Glit0.nodes(data=True)), orient='index')
 ref_attr0 = ref_attr0.merge(attr_df_uni)
 
 # Speichern als Objekt
-os.chdir("P:\SWITCHdrive\Datenauswertung\Python_Zeug\Zitationsnetzwerke\Outputs\Objects")
 with open('Refattribute_com0', 'wb') as ref_attr0_file:
     pickle.dump(ref_attr0, ref_attr0_file)
 
@@ -316,7 +279,6 @@ reftyp0 = ref_attr0['Reftyp'].value_counts()
 reftyp0_prop = (round(ref_attr0['Reftyp'].value_counts(normalize = True), 2)*100).astype(int)
 
 ############## Literaturnetzwerk des 2. Subgraphen (Community) ###############
-os.chdir("P:\SWITCHdrive\Datenauswertung\Python_Zeug\Zitationsnetzwerke\Outputs\Objects")
 with open('Sub_Com2', 'rb') as Sub_Com2_file:
     subG1 = pickle.load(Sub_Com2_file)
 
@@ -331,7 +293,6 @@ degree_dict1 = dict(Glit1.degree(Glit1.nodes))
 nx.set_node_attributes(Glit1, degree_dict1, 'degree')
 
 # Speichern des Literaturnetzwerkes des 2. Subgraphen
-os.chdir("P:\SWITCHdrive\Datenauswertung\Python_Zeug\Zitationsnetzwerke\Outputs\Objects") 
 with open('LitGraph_com1', 'wb') as Glit1_file:
     pickle.dump(Glit1, Glit1_file)
 
@@ -354,7 +315,6 @@ top5_1 = pd.DataFrame(
      'Gradzahl': grad1,
     })
 
-os.chdir("P:\SWITCHdrive\Datenauswertung\Python_Zeug\Zitationsnetzwerke\Outputs\Tables")
 top5_1.to_csv("20192801_Top5_1.csv", header=True, encoding='utf-8-sig', index=False)
 
 # closeness
@@ -366,15 +326,11 @@ print("Top 20 nodes by Closeness Centrality:")
 for d in sorted_degree1[:20]:
     print(d)
 
-# Gephi
-#os.chdir("P:\SWITCHdrive\Datenauswertung\Python_Zeug\Zitationsnetzwerke\Outputs\Gephi")
-#nx.write_graphml(Glit1, "20192801_Lit_SubG1.graphml")
-
 # Zentralitätsmasse der Knoten des Subgraphen der Community 2
 lit1_counts = pd.DataFrame.from_dict(dict(Glit1.nodes(data=True)), orient='index')
 lit1_counts = lit1_counts.drop(['bipartite'], axis =1).reset_index().sort_values(by=['degree'], ascending=False).rename(columns={'degree':'Grad', 'index': 'Quelle'})
 lit1_counts['Closeness'] = round(lit1_counts['Closeness'], 2)
-os.chdir("P:\SWITCHdrive\Datenauswertung\Python_Zeug\Zitationsnetzwerke\Outputs\Tables")
+
 lit1_counts.to_csv("20192801_Lit1_measures.csv", header=True, encoding='utf-8-sig', index=False)
 
 ###### Statistiken zur Literatur des Clusters
@@ -383,7 +339,6 @@ ref_attr1 = pd.DataFrame.from_dict(dict(Glit1.nodes(data=True)), orient='index')
 ref_attr1 = ref_attr1.merge(attr_df_uni)
 
 # Speichern
-os.chdir("P:\SWITCHdrive\Datenauswertung\Python_Zeug\Zitationsnetzwerke\Outputs\Objects")
 with open('Refattribute_com1', 'wb') as ref_attr1_file:
     pickle.dump(ref_attr1, ref_attr1_file)
 
@@ -393,7 +348,6 @@ reftyp1 = ref_attr1['Reftyp'].value_counts()
 reftyp1_prop = (round(ref_attr1['Reftyp'].value_counts(normalize = True), 2)*100).astype(int)
 
 ############## Literaturnetzwerk des 3. Subgraphen (Community) ###############
-os.chdir("P:\SWITCHdrive\Datenauswertung\Python_Zeug\Zitationsnetzwerke\Outputs\Objects")
 with open('Sub_Com3', 'rb') as Sub_Com3_file:
     subG2 = pickle.load(Sub_Com3_file)
 
@@ -408,7 +362,6 @@ degree_dict2 = dict(Glit2.degree(Glit2.nodes))
 nx.set_node_attributes(Glit2, degree_dict2, 'degree')
 
 # Speichern des Literaturnetzwerkes des 3. Subgraphen
-os.chdir("P:\SWITCHdrive\Datenauswertung\Python_Zeug\Zitationsnetzwerke\Outputs\Objects") 
 with open('LitGraph_com2', 'wb') as Glit2_file:
     pickle.dump(Glit2, Glit2_file)
 
@@ -431,7 +384,6 @@ top5_2 = pd.DataFrame(
      'Gradzahl': grad2,
     })
 
-os.chdir("P:\SWITCHdrive\Datenauswertung\Python_Zeug\Zitationsnetzwerke\Outputs\Tables")
 top5_2.to_csv("20192801_Top5_2.csv", header=True, encoding='utf-8-sig', index=False)
 
 # closeness
@@ -443,15 +395,11 @@ print("Top 20 nodes by Closeness Centrality:")
 for d in sorted_degree2[:20]:
     print(d)
 
-# Für Gephi speichern
-#os.chdir("P:\SWITCHdrive\Datenauswertung\Python_Zeug\Zitationsnetzwerke\Outputs\Gephi")
-#nx.write_graphml(Glit2, "20192801_Lit_SubG2.graphml")
-
 # Für Visualisierung
 lit2_counts = pd.DataFrame.from_dict(dict(Glit2.nodes(data=True)), orient='index')
 lit2_counts = lit2_counts.drop(['bipartite'], axis =1).reset_index().sort_values(by=['degree'], ascending=False).rename(columns={'degree':'Grad', 'index': 'Quelle'})
 lit2_counts['Closeness'] = round(lit2_counts['Closeness'], 2)
-os.chdir("P:\SWITCHdrive\Datenauswertung\Python_Zeug\Zitationsnetzwerke\Outputs\Tables")
+
 lit2_counts.to_csv("20192801_Lit2_measures.csv", header=True, encoding='utf-8-sig', index=False)
 
 ###### Statistiken zur Literatur des Clusters
@@ -460,7 +408,6 @@ ref_attr2 = pd.DataFrame.from_dict(dict(Glit2.nodes(data=True)), orient='index')
 ref_attr2 = ref_attr2.merge(attr_df_uni)
 
 # Für die Visualisierung
-os.chdir("P:\SWITCHdrive\Datenauswertung\Python_Zeug\Zitationsnetzwerke\Outputs\Objects")
 with open('Refattribute_com2', 'wb') as ref_attr2_file:
     pickle.dump(ref_attr2, ref_attr2_file)
 
@@ -468,36 +415,3 @@ grad_schnitt2 = (ref_attr2['degree'].sum() / len(ref_attr2)).astype(int)
 close_schnitt2 = round(ref_attr2['Closeness'].sum() / len(ref_attr2), 2)
 reftyp2 = ref_attr2['Reftyp'].value_counts()
 reftyp2_prop = (round(ref_attr2['Reftyp'].value_counts(normalize = True), 2)*100).astype(int)
-
-###########################################################################
-
-# Überschneidungen der Literaturnetzwerke
-#print(set(lit0).intersection(lit1))
-#print(set(lit0).intersection(lit2))
-#print(set(lit1).intersection(lit2))
-
-# =============================================================================
-#       Vergleich mit Gesamtnetzwerk (nicht in Retreatartikel aufgenommen)
-# =============================================================================
-#Glit = [x for x,y in B.nodes(data=True) if y['bipartite']==1]
-#Glit = bi.weighted_projected_graph(B, Glit,ratio=False)
-#
-#communities_lit = community.best_partition(Glit)
-## Anzahl der "communities"
-#set(communities_lit.values()) # 11 Communities
-#nx.set_node_attributes(Glit, communities_lit, 'modularity')
-#nx.write_graphml(Glit, "170118 Graph Literatur.graphml")
-#density_lit = round(nx.density(Glit), 2)
-#
-## Nach Blick in Gephi: Ähnliche Unterteilung bezüglich der grossen Blöcke,
-## nur eben mit einzelnen separaten Lehrplänen als einzelne Cluster.
-#
-## Nach degree Filtern, um solche Insel herauszubekommen.
-#degree_dict = dict(Glit.degree(Glit.nodes))
-#nx.set_node_attributes(Glit, degree_dict, 'degree')
-#nx.get_node_attributes(Glit, 'degree')
-#
-## Zur Bestimmung des Schwellenwertes durchschnittliche Reflänge heranziehen?
-#selected_nodes = [n for n,v in Glit.nodes(data=True) if v['degree'] > 20]
-#len(selected_nodes)
-#Gfilter = Glit.subgraph(selected_nodes)
